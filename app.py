0712bd010ddb9797b0c909bfd16d67b9b5d15881
sahil102103 <sahilshah102103@gmail.com>
2023-05-27 13:57:25 -0400

    fixed routing

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
 import requests
-import json
-import pandas as pd
-from flask import Flask, render_template, request, redirect, flash, session, url_for
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 # Get all the data from the API
 url = 'http://api.nessieisreal.com/enterprise/accounts?key=e1cac451319e9263333280ff4a5b28f6'
 response = requests.get(url)
-data = response.json()
+data = response.json()['results']  # Access the 'results' key in the JSON response
 
 # Define home page route
-# When the URL is accessed, the index function will run,
-# which will render the index.html template with the variable account_found=False
 @app.route('/')
 def index():
     return render_template('login.html', account_found=False)
 
 # Define logout route to handle logout action
-# When the URL is called with /logout, the logout function will be called
-# which will lead the user to the base URL
 @app.route('/logout')
 def logout():
     return redirect('/')
@@ -32,17 +26,19 @@ def login():
     nickname = ''
     balance = 0
     # Get the account ID from the form
-    # request.form is a dictionary that contains the form data
-    id = request.form['id']
+    id = request.form["id"]
     # Loop through the data to find the account with the ID
     for account in data:
-        if account['id'] == id:
+        if account["_id"] == id:
             account_found = True
-            nickname = account['nickname']
-            balance = account['balance']
+            nickname = account["nickname"]
+            balance = account["balance"]
             break
-    # If account is found, render the account.html template with the account details
+    # If account is found, render the index.html template with the account details
     if account_found:
         return render_template('index.html', nickname=nickname, balance=balance, account_found=True)
     else:
         return render_template('login.html', account_found=False)
+
+if __name__ == "__main__":
+    app.run()
