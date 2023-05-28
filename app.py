49de5f64fe88d0238856a2bd20b95af7a2074d18
sahil102103 <sahilshah102103@gmail.com>
2023-05-27 22:11:18 -0400

    fine tuned GPT

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 MAX_RESPONSE_LENGTH = 500  # Define the maximum length of the response to display
 
 # Get all the data from the API
@@ -52,7 +52,6 @@ def login():
     else:
         return render_template('login.html', account_found=False)
 
-
 @app.route("/openaifunc", methods=["GET", "POST"])
 def openaifunc():
     nickname = ''
@@ -62,11 +61,13 @@ def openaifunc():
         human = request.form["human"]
         response = openai.Completion.create(
             engine="text-davinci-003",
-            prompt=generate_prompt(human),
+            prompt=generate_prompt(human, nickname, balance),
             temperature=0.6,
-            max_tokens=MAX_RESPONSE_LENGTH,  # Limit the response length
+            max_tokens=MAX_RESPONSE_LENGTH,
         )
         result = truncate_text(response.choices[0].text)
+
+        # Retrieve the ID from the session
         id = session.get("id")
         if id:
             for account in data:
@@ -95,30 +96,45 @@ def openaifunc():
     return render_template("index.html", nickname=nickname, balance=balance, result=result)
 
 
-def generate_prompt(human):
-    return """Suggest financial advice for a human living in the United States. Provide detailed recommendations based on different financial situations, including leveraging specific Capital One services.
+def generate_prompt(human, nickname, balance):
+    nickname = ''
+    balance = 0
 
-Human: I am struggling to manage my expenses.
-Response: If you're having difficulty managing expenses, consider using Capital One's budgeting tools like the Capital One Mobile App to track your spending. Create a detailed budget that includes essential expenses and discretionary spending. Reach out to Capital One's customer support for personalized guidance.
+        # Retrieve the ID from the session
+    id = session.get("id")
+    if id:
+        for account in data:
+            if account["_id"] == id:
+                old_nickname = account["nickname"]
+                nickname = old_nickname.replace("'s Account", "")
 
-Human: I want to start saving for the future.
-Response: To begin saving for the future, open a Capital One 360 Savings Account. Set specific savings goals and establish automatic transfers from your checking account. Capital One provides online tools to monitor your progress. Explore Capital One's investment options to grow your savings.
+                oldbalance = account["balance"]
+                balance = "{:.2f}".format(oldbalance)
+                break
 
-Human: I am considering purchasing a home.
-Response: If you're planning to buy a home, reach out to Capital One Home Loans for mortgage options and assistance. Use Capital One's mortgage calculator to estimate payments. Capital One's Home Loans team can guide you through the process, including pre-qualification, application, and closing.
 
-Human: I am interested in investing in the stock market.
-Response: If you're considering investing in the stock market, Capital One Investing provides a platform for online trading and investment research. Open a Capital One Investing account to access a wide range of investment options. Utilize Capital One's research tools and educational resources. Consult with Capital One's investment advisors for personalized guidance.
+    return f"""Suggest financial advice for {nickname} with a balance of {balance}. Provide detailed recommendations based on different financial situations, including leveraging specific Capital One services.
 
-Human: I have 0 dollars in my bank account.
-Response: Focus on saving money diligently. Consider opening a Capital One 360 Savings Account. Create a budget to track expenses and identify areas to cut back. Explore government assistance programs. Look for ways to reduce costs on essential items. Gradually build an emergency fund.
+    Human: What is my account balance
+    Response: Dear {nickname}, your account balance is {balance} dollars.
+    
+    Human: I am struggling to manage my expenses.
+    Response: If you're having difficulty managing expenses {nickname}, consider using Capital One's budgeting tools like the Capital One Mobile App to track your spending. Create a detailed budget that includes essential expenses and discretionary spending. Reach out to Capital One's customer support for personalized guidance.
 
-Human: I have 10,000,000 dollars in my bank account.
-Response: Partner with Capital One's Private Wealth Management team to develop a comprehensive investment strategy. Allocate funds towards long-term investments like stocks, bonds, and real estate. Consider retirement savings, tax planning, and philanthropic opportunities.
+    Human: I want to start saving for the future.
+    Response: To begin saving for the future {nickname}, open a Capital One 360 Savings Account. Set specific savings goals and establish automatic transfers from your checking account. Capital One provides online tools to monitor your progress. Explore Capital One's investment options to grow your savings.
 
-Human: {}
-Response:""".format(human.capitalize())
+    Human: I am considering purchasing a home.
+    Response: If you're planning to buy a home {nickname}, reach out to Capital One Home Loans for mortgage options and assistance. Use Capital One's mortgage calculator to estimate payments. Capital One's Home Loans team can guide you through the process, including pre-qualification, application, and closing.
 
+    Human: I am interested in investing in the stock market.
+    Response: If you're considering investing in the stock market {nickname}, Capital One Investing provides a platform for online trading and investment research. Open a Capital One Investing account to access a wide range of investment options. Utilize Capital One's research tools and educational resources. Consult with Capital One's investment advisors for personalized guidance.
+
+    Human: I have 0 dollars in my bank account.
+    Response: Focus on saving money diligently {nickname}. Consider opening a Capital One 360 Savings Account. Create a budget to track expenses and identify areas to cut back. Explore government assistance programs. Look for ways to reduce costs on essential items. Gradually build an emergency fund.
+
+    Human: {human}
+    Response:""".format(nickname=nickname)
 
 def truncate_text(text):
     if len(text) > MAX_RESPONSE_LENGTH:
